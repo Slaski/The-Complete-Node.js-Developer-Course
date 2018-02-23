fe9d9eff763bde23ffb4b2f61fb125319b7f8ea6
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-02-23 19:34:25 -0300

    feat: Added a 3rd party module.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,13 +2,18 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
+
+console.log(_.isString(2));
+console.log(_.isString('test'));
+console.log(_.uniq([1, 2, 3, 3, 2, 1]));
 
 console.log(notes.add(1, 2));
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.`, err => {
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
     if (err) {
         console.log('Unable to write to file.');
     }
