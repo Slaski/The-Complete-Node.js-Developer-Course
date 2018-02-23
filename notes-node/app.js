9bdf2f0d3023816e87900e0e3aa817597892466d
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-02-23 17:58:24 -0300

    feat: Implemented the first own module and imported it.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,13 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
+
+console.log(notes.add(1, 2));
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.`, err => {
     if (err) {
         console.log('Unable to write to file.');
     }
---
+++ notes-node/notes.js
@@ -0,0 +1,10 @@
+console.log('Stating notes.js.');
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    // return 'New note'
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
