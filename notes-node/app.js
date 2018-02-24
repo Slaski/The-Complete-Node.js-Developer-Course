458397e63849ea64eea447259abe908cadf779b3
Slaski <slaski@protonmail.com>
2018-02-24 14:43:49 -0300

    feat: Implemented the yargs module to handle arguments.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,26 +2,27 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
-console.log(process.argv);
 
+const argv = yargs.argv;
 let command = process.argv[2];
 console.log(`Command: ${command}`);
 
 switch(command) {
     case 'add':
-        console.log('Adding new note.');
+        notes.addNote(argv.title, argv.body);
         break;
     case 'list':
-        console.log('Listing all notes.');
+        notes.getAll();
         break;
     case 'read':
-        console.log('Reading a note.');
+        notes.getNote(argv.title);
         break;
     case 'remove':
-        console.log('Removing note.');
+        notes.removeNote(argv.title);
         break;
     default:
         console.log('Command not recognized.');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,24 @@
 console.log('Stating notes.js.');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note'
+let addNote = (title, body) => {
+    console.log('Adding note:', title, body);
+};
+
+let getAll = () => {
+    console.log('Getting all notes.');
+};
+
+let getNote = (title) => {
+    console.log('Reading note', title);
+};
+
+let removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
