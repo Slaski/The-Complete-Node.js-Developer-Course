e6253af7b405665faaba44dea16500004c9bb706
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-02-27 19:15:22 -0300

    feat: Implemented a call to the Google Geocoding API.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,12 @@
 const request = require('request');
+const key = require('./key.js');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=rua%20bar%C3%A3o%20de%20campinas%20441',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=rua%20bar%C3%A3o%20de%20campinas%20441&key=${key}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    var result = body.results[0];
+    console.log(`Address: ${result.formatted_address}`);
+    console.log(`Latitude: ${result.geometry.location.lat}`);
+    console.log(`Longitude: ${result.geometry.location.lng}`);
 });
