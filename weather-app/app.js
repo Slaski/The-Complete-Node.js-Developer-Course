1550a28441960681866cc52a2edea71d32b9f40f
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-02-28 15:00:52 -0300

    feat: Implemented simple error handling for the requests and added the yargs module for the weather app.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
 const key = require('./key.js');
 
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=rua%20bar%C3%A3o%20de%20campinas%20441&key=${key}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${key}`,
     json: true
 }, (error, response, body) => {
-    var result = body.results[0];
-    console.log(`Address: ${result.formatted_address}`);
-    console.log(`Latitude: ${result.geometry.location.lat}`);
-    console.log(`Longitude: ${result.geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        var result = body.results[0];
+        console.log(`Address: ${result.formatted_address}`);
+        console.log(`Latitude: ${result.geometry.location.lat}`);
+        console.log(`Longitude: ${result.geometry.location.lng}`);
+    }
 });
