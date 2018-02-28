b650ac7d80cb081441b91b923dc0b1ac0e90c3e4
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-02-28 17:04:32 -0300

    feat: Played with chain promises on the playground.

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,12 +1,20 @@
-var somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve('Hey. It worked!');
-        reject('Unable to fulfill promise.');
-    }, 2500);
-});
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 2500);
+    });
+};
 
-somePromise.then((message) => {
-    console.log('Success:', message);
-}, (errorMessage) => {
-    console.log('Error:', errorMessage);
+asyncAdd(5, 7).then((result) => {
+    console.log('Result:', result);
+    return asyncAdd(result, '33');
+}).then((result) => {
+    console.log('Result:', result);
+}).catch(errorMessage => {
+    console.log(errorMessage);
 });
