cfbf57976b9b685d025a97bdc956d4e7d522ba86
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-02 16:39:32 -0300

    feat: Added and tested async functions.

--- nodes-test/utils/utils.js
+++ nodes-test/utils/utils.js
@@ -1,5 +1,19 @@
 module.exports.add = (a, b) => a + b;
+
+module.exports.asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+};
+
 module.exports.square = (x) => x * x;
+
+module.exports.asyncSquare = (x, callback) => {
+    setTimeout(() => {
+        callback(x * x);
+    }, 1000);
+};
+
 module.exports.setName = (user, fullName) => {
     var names = fullName.split(' ');
     user.firstName = names[0];
--- nodes-test/utils/utils.test.js
+++ nodes-test/utils/utils.test.js
@@ -7,10 +7,24 @@ it('should add two numbers', () => {
     expect(result).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (result) => {
+        expect(result).toBe(7).toBeA('number');
+        done();
+    });
+});
+
 it('should square a number', () => {
     var result = utils.square(3);
     expect(result).toBe(9).toBeA('number');
 });
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(4, (result) => {
+        expect(result).toBe(16).toBeA('number');
+        done();
+    });
+})
 
 it('should verify first and last names are set', () => {
     var result = utils.setName({}, 'Rodrigo Zanetta');
