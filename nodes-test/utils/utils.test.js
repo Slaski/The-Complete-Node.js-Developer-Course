56e08fabdd7f1de6d93640adde81faa85926e9ad
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-02 16:10:57 -0300

    feat: Added an assertion module
    Added the module expect, an assertion library.

--- nodes-test/utils/utils.test.js
+++ nodes-test/utils/utils.test.js
@@ -1,15 +1,13 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var result = utils.add(33, 11);
-    if (result !== 44) {
-        throw new Error(`Expected 44, but got ${result}.`);
-    }
+    expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var result = utils.square(3);
-    if (result !== 9) {
-        throw new Error(`Expected 9, but got ${result}.`);
-    }
+    expect(result).toBe(9).toBeA('number');
 });
