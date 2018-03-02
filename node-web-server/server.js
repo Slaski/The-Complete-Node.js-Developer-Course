6719061c78ffc9a077c5d6c8bf93b964fa481144
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-02 10:20:46 -0300

    feat: Added static files and the hbs module to render templates and partials.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (request, response) => {
-    // response.send('<h1>Hello Express!</h1>');
-    response.send({
-        name: 'Slaski',
-        age: 28
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
     });
 });
 
 app.get('/about', (request, response) => {
-    response.send('About Page');
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (request, response) => {
@@ -20,4 +34,6 @@ app.get('/bad', (request, response) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
