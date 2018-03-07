dbf12f9d510f62459d973eea8b69def4c2ad665c
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-07 18:47:35 -0300

    chore: Added the configurations for deplying on heroku.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -7,6 +7,8 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.get('/todos', (req, res) => {
@@ -47,8 +49,8 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000.');
+app.listen(port, () => {
+    console.log(`Started on port ${port}.`);
 });
 
 module.exports = {app};
