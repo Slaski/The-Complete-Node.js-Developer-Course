d77fd55f447fcc1d2595a0f4f060e0b63762d3e2
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-07 11:30:49 -0300

    refactor: User destructure to get the MongoClient in the playground.

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
 
 MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
     if (err) {
