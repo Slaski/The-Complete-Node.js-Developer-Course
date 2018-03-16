533ee49dde7c03b80781232ff72ef804da77345b
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-16 16:58:17 -0300

    feat: Added the POST /users route.

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -97,6 +97,19 @@ app.patch('/todos/:id', (req, res) => {
         });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save()
+        .then(user => {
+            res.send({user});
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}.`);
 });
