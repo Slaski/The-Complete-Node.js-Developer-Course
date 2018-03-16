af70e48d1a97c9bc9cd80e3f80901648ad7b4264
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-16 12:20:21 -0300

    chore: Setup separate test and development envs.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+require('./config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
