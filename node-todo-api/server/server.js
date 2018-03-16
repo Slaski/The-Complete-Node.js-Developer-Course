f9b930e53fae2351b73b44032477247a51134c3b
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-16 10:49:23 -0300

    feat: Added the PATCH /todos/id route.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -65,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         return res.status(400).send(err);
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['completed', 'text']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send(todo);
+        })
+        .catch(err => {
+            res.status(400).send();
+        });
 });
 
 app.listen(port, () => {
