b6532807cf844801001bc8ba93226c057ce94a64
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-19 20:58:06 -0300

    feat: Hash user password before saving.

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -26,12 +27,27 @@ const jwt = require('jsonwebtoken');
 //     console.log('Data was changed. Do not trust.');
 // }
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var data = {
+//     id: 10
+// };
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+
+
+var password = '12345678';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$l0py6WtwX9DC5RadHhnPQe9nLW34EsTnfi5cBvrO.lqnyWZDOocm.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
