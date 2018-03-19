97531d9d59b1b750ce4a7389e01f325160ff366b
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-19 11:46:35 -0300

    feat: Add GET /users/me and authentication middleware.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -111,6 +112,10 @@ app.post('/users', (req, res) => {
         .catch(err => {
             res.status(400).send(err);
         });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
