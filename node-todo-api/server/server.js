c2943204e9c1dafca6007b64df97f688eca61b36
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-20 11:01:54 -0300

    feat: Added the DELETE /users/me/token route.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -132,6 +132,16 @@ app.post('/users/login', (req, res) => {
         });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token)
+        .then(() => {
+            res.send();
+        })
+        .catch(err => {
+            res.status(400).send();
+        });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}.`);
 });
