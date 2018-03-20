c8e4b3da6266112d89a7766e4003537d7a9dc835
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-20 10:54:36 -0300

    test: Added the tests for the POST /users/login route.

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -240,4 +240,62 @@ describe('server.js', () => {
                 .end(done);
         });
     });
+
+    describe('POST /users/login', () => {
+        it('should login user and return auth token', (done) => {
+            var {email, password} = users[1];
+
+            request(app)
+                .post('/users/login')
+                .send({email, password})
+                .expect(200)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).toExist();
+                })
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    User.findById(users[1]._id)
+                        .then((user) => {
+                            expect(user.tokens[0]).toInclude({
+                                access: 'auth',
+                                token: res.headers['x-auth']
+                            });
+                            done();
+                        })
+                        .catch((err) => {
+                            done(err);
+                        });
+                });
+        });
+
+        it('should reject invalid login', (done) => {
+            var email = users[1].email;
+            var password = users[1].password + '1';
+
+            request(app)
+                .post('/users/login')
+                .send({email, password})
+                .expect(400)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).toNotExist();
+                })
+                .end((err) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    User.findById(users[1]._id)
+                        .then((user) => {
+                            expect(user.tokens.length).toBe(0);
+                            done();
+                        })
+                        .catch((err) => {
+                            done(err);
+                        });
+                });
+        });
+    });
 });
