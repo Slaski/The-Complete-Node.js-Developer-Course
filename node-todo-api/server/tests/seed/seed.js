38ba83371d92ac90be239f7ca25ba25e0a25c4e2
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-20 07:22:27 -0300

    refactor: Refactored the seed database functions for the tests to a separate file.
    Also added the users seed function.

---
+++ node-todo-api/server/tests/seed/seed.js
@@ -0,0 +1,55 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo, User} = require('./../../models');
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+}];
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'slaski@example.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'rodrigo@example.com',
+    password: 'userTwoPass'
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({})
+        .then(() => {
+            return Todo.insertMany(todos);
+        })
+        .then(() => done())
+        .catch(err => {
+            console.log('Error populating the Todos test database.');
+            console.log(err);
+        });
+};
+
+const populateUsers = (done) => {
+    User.remove({})
+        .then(() => {
+            var userOne = new User(users[0]).save();
+            var userTwo = new User(users[1]).save();
+            return Promise.all([userOne, userTwo]);
+        })
+        .then(() => done())
+        .catch(err => console.log(err));
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
