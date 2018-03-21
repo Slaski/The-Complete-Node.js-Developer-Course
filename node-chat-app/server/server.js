708600ab486f638164c98ab29c6b2d7bffb69ade
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-21 17:45:15 -0300

    feat: Added messages for when a user joins the chat.

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -21,9 +21,21 @@ io.on('connection', socket => {
         console.log('User was disconnected');
     });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined.',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log(message);
-        io.emit('newMessage', {
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
