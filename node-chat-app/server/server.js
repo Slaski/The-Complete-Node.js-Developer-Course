2353c2e3acfac0bce2a116dbd8d281c99556d82f
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-21 11:59:17 -0300

    chore: Added Socket.io and explored its events.

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -2,14 +2,27 @@ require('./config');
 
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './../public');
 const port = process.env.PORT;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started listening on port ${port}.`);
 })
 
