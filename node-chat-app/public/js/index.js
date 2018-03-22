546a70dd0ecdb24933eb0728fc6383fc0714852e
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-22 16:38:44 -0300

    chore: Added moment and mustache to render the messages.

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -9,22 +9,36 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
-    
-    var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`)
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>');
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
+    // var li = $('<li></li>');
+    // var a = $('<a target="_blank">My current location</a>');
+    //
+    // li.text(`${message.from} (${formattedTime}): `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // $('#messages').append(li);
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
 });
 
 $('#message-form').on('submit', function(e) {
