bc865f48e89b941373a3d3f56d7794ceaecd684d
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-03-22 17:33:32 -0300

    feat: Scroll bottom if user is close to bottom.

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -18,6 +33,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -30,6 +46,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -27,7 +27,6 @@ io.on('connection', socket => {
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
     socket.on('createMessage', (message, callback) => {
-        console.log(message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
     });
